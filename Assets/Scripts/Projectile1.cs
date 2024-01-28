25db124f0c5e0f1fa0e59dcb06c8800d396ccf98
Jason Zheng <zhengj8@rpi.edu>
2024-01-28 00:29:14 -0500

    sound files and rename pie to projectile1

--- Assets/Scripts/Projectile1.cs
+++ Assets/Scripts/Projectile1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pie : MonoBehaviour
+public class Projectile1 : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
     [SerializeField] private Rigidbody2D _rb;
