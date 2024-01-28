70a72bbcef13e4dbb6e8d4f4aed5e5d57009cdc7
Xeshe <kingdomheartskeyblade137@gmail.com>
2024-01-28 14:38:08 -0500

    level1 tilemap and integration with UI
    finished level1 tilemap, added UI from UI test scene with pause menu, and card functionality

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
@@ -7,16 +9,32 @@ public class UIManager : MonoBehaviour
     public static UIManager Instance;
     [SerializeField] private bool gamePaused = false;
     [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject effectsCanvas;
+    [SerializeField] private int balloons;
+    [SerializeField] private TextMeshProUGUI balloonText;
 
     private void Start()
     {
         Instance = this;
         UnpauseGame();
     }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gamePaused)
+            {
+                UnpauseGame() ;
+            }
+            else { PauseGame(); }
+        }   
+    }
     public void PauseGame()
     {
         gamePaused = true;
         pauseMenu.SetActive(true);
+        Time.timeScale = 0f; 
 
     }
 
@@ -24,5 +42,21 @@ public class UIManager : MonoBehaviour
     {
         gamePaused = false;
         pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void UpdateBalloonCounter(int balloons)
+    {
+        balloonText.text = "Balloons: " + balloons.ToString();
+    }
+
+    public void ShowEffectsCanvas()
+    {
+        effectsCanvas.SetActive(true);
+    }
+
+    public void HideEffectsCanvas()
+    {
+        effectsCanvas.SetActive(false);
     }
 }
